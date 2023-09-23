56cf09386c1014d6fd1b18d9b6429f75a93f2fbb
Long Vu Dang <74718176+longgggg1310@users.noreply.github.com>
2023-09-23 20:53:27 +0700

    first

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+//init db
+
+//init routes
+app.get("/", (req, res, next) => {
+  const strCompress = "123";
+  return res.status(200).json({
+    message: "OK",
+    metadata: strCompress.repeat(10000),
+  });
+});
+// handler error
+
+module.exports = app;
