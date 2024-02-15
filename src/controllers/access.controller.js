b5e0300bb67a2b59f7fe67fa419e9f057b3cbd00
minh.lucvan <long.vu190404@vnuk.edu.vn>
2024-02-15 18:03:53 +0700

    update hander error

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -2,6 +2,7 @@
 const AccessService = require("../services/access.service");
 class AccessController {
   signUp = async (req, res, next) => {
+    
     return res.status(201).json(await AccessService.signUp(req.body));
   };
 }
